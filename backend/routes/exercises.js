a8be8dd2eae5ea025e1f65920aef6f82b1559142
Kevin Fang <kevin21@mit.edu>
2020-06-14 11:25:32 -0600

    idk do what you want

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -14,6 +14,11 @@ router.route('/add').post((req, res) => {
     const duration = req.body.duration;
     const yardage = req.body.yardage;
     const date = Date.parse(req.body.date);
+    DESCRIPTIONS = ["Swimming"]
+    if (!DESCRIPTIONS.includes(description)) {
+        res.status(400).json("wtf you trolling bro");
+    }
+        
 
     const newExercise = new Exercise({
         username,
